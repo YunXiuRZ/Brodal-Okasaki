f6e7718c3fc30ba515e5c1672412978c7d9f37d5
YunXiuRZ <70698588+YunXiuRZ@users.noreply.github.com>
2024-08-01 16:12:39 +0800

    Add "PriorityQueue" typeclass

--- SkewBinomialHeap.hs
+++ SkewBinomialHeap.hs
@@ -1,10 +1,5 @@
 module SkewBinomialHeap 
 ( Ts
-, isEmpty
-, insert
-, merge
-, findMin
-, deleteMin
 , size) where
 import PriorityQueue
 
@@ -42,43 +37,45 @@ skewLink t0@(Node x0 r0 _) t1@(Node x1 r1 c1) t2@(Node x2 r2 c2)
 
 -- We assume that rank t <= tank t'
 insertTree :: Ord a => STree a -> Ts a -> Ts a
-insertTree t [] = [t]
-insertTree t (t':ts)
-    | rank t < rank t' = t:t':ts
-    | otherwise        = insertTree (link t t') ts
+insertTree t (Build []) = Build [t]
+insertTree t (Build (t':ts))
+    | rank t < rank t' = Build (t:t':ts)
+    | otherwise        = insertTree (link t t') (Build ts)
 
 -- Link them if there are two lowest-rank tree
 uniqify :: Ord a => Ts a -> Ts a
-uniqify [] = []
-uniqify (t:ts) = insertTree t ts
+uniqify (Build []) = Build []
+uniqify (Build (t:ts)) = insertTree t (Build ts)
 
 meldUniq :: Ord a => Ts a -> Ts a -> Ts a
-meldUniq [] ts = ts
-meldUniq ts [] = ts
-meldUniq (t1:ts1) (t2:ts2)
-    | rank t1 < rank t2 = t1:meldUniq ts1 (t2:ts2)
-    | rank t2 < rank t1 = t2:meldUniq (t1:ts1) ts2
-    | otherwise         = insertTree (link t1 t2) (meldUniq ts1 ts2)
+meldUniq (Build []) ts = ts
+meldUniq ts (Build []) = ts
+meldUniq ts1@(Build (t1:tss1)) ts2@(Build (t2:tss2))
+    | rank t1 < rank t2 = let (Build ts') = meldUniq (Build tss1) ts2
+                            in Build (t1:ts')
+    | rank t2 < rank t1 = let (Build ts') = meldUniq ts1 (Build tss2)
+                            in Build (t2:ts')
+    | otherwise         = insertTree (link t1 t2) (meldUniq (Build tss1) (Build tss2))
 
 instance PriorityQueue Ts where
-    empty = []
+    empty = Build []
 
     isEmpty :: Ts a -> Bool
-    isEmpty = null
+    isEmpty (Build ts) = null ts
 
     insert :: Ord a => a -> Ts a -> Ts a
-    insert x ts@(t1:t2:tss)
-        | rank t1 == rank t2 = skewLink (Node x 0 []) t1 t2 : tss
-        | otherwise          = Node x 0 [] : ts
-    insert x ts = Node x 0 [] : ts
+    insert x ts@(Build (t1:t2:tss))
+        | rank t1 == rank t2 = Build (skewLink (Node x 0 []) t1 t2 : tss)
+        | otherwise          = Build (Node x 0 [] : (t1:t2:tss))
+    insert x (Build ts) = Build (Node x 0 [] : ts)
 
     merge :: Ord a => Ts a -> Ts a -> Ts a
     merge ts ts' = meldUniq (uniqify ts) (uniqify ts')
 
     -- I don't want to deal with side effect in agda so I use "Nothing" rather than exception
     findMin :: Ord a => Ts a -> Maybe a
-    findMin [] = Nothing
-    findMin (t:ts) = case findMin ts of
+    findMin (Build []) = Nothing
+    findMin (Build (t:ts)) = case findMin (Build ts) of
         Nothing              -> Just (root t)
         Just v | root t <= v -> Just (root t)
         Just v | otherwise   -> Just v
@@ -88,20 +85,20 @@ instance PriorityQueue Ts where
     -- their rank. The former group contains subtrees with rank > 0 
     -- while the latter contains subtrees with rank = 0.
     deleteMin :: Ord a => Ts a -> Ts a
-    deleteMin [] = []
-    deleteMin ts = foldr insert (meld tss ts') xs'
+    deleteMin (Build []) = empty
+    deleteMin ts = foldr insert (merge tss ts') xs'
         where   getMin :: Ord a => Ts a -> (STree a, Ts a)
-                getMin [t] = (t, [])
-                getMin (t:ts)
-                    | root t <= root t' = (t, ts)
-                    | otherwise         = (t', t:ts')
-                    where (t', ts') = getMin ts
+                getMin (Build [t]) = (t, empty)
+                getMin (Build (t:ts))
+                    | root t <= root t' = (t, Build ts)
+                    | otherwise         = (t', Build (t:ts'))
+                    where (t', Build ts') = getMin (Build ts)
                 split :: Ts a -> [a] -> Ts a -> (Ts a, [a])
-                split ts xs [] = (ts, xs)
-                split ts xs (t:c)
-                    | rank t == 0 = split ts (root t : xs) c
-                    | otherwise   = split (t:ts) xs c
+                split ts xs (Build []) = (ts, xs)
+                split (Build ts) xs (Build (t:c))
+                    | rank t == 0 = split (Build ts) (root t : xs) (Build c)
+                    | otherwise   = split (Build (t:ts)) xs (Build c)
                 (Node x r c, tss) = getMin ts
-                (ts', xs')        = split [] [] c
+                (ts', xs')        = split empty [] (Build c)
 
 
--- SkewBinomialHeapTest.hs
+++ SkewBinomialHeapTest.hs
@@ -1,7 +1,8 @@
+import PriorityQueue
 import SkewBinomialHeap
 
 toSkewBinomialHeap :: Ord a => [a] -> Ts a
-toSkewBinomialHeap = foldr insert []
+toSkewBinomialHeap = foldr insert empty
 
 heap :: Ts Integer
 heap = toSkewBinomialHeap [4, 8, 7, 6, 3]
